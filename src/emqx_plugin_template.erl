3e809dff25bf84adc413ea65c337fb4c831d95ed
renfubiao <renfubiao@abupdate.com>
2023-05-15 21:10:58 +0800

    feat: add common config

--- src/emqx_plugin_template.erl
+++ src/emqx_plugin_template.erl
@@ -29,12 +29,8 @@
         ]).
 
 %% Client Lifecircle Hooks
--export([ on_client_connect/3
-        , on_client_connack/4
-        , on_client_connected/3
+-export([ on_client_connected/3
         , on_client_disconnected/4
-        , on_client_authenticate/3
-        , on_client_authorize/5
         , on_client_subscribe/4
         , on_client_unsubscribe/4
         ]).
@@ -43,18 +39,20 @@
 -export([ on_session_created/3
         , on_session_subscribed/4
         , on_session_unsubscribed/4
-        , on_session_resumed/3
-        , on_session_discarded/3
-        , on_session_takeovered/3
-        , on_session_terminated/4
         ]).
 
 %% Message Pubsub Hooks
 -export([ on_message_publish/2
-        , on_message_delivered/3
-        , on_message_acked/3
-        , on_message_dropped/4
         ]).
+
+func([]) -> ok;
+
+func([H | T]) ->
+    MqttTopic = maps:get(<<"mqtt_topic">>, H),
+    KafkaTopic = maps:get(<<"kafka_topic">>, H),
+    ?SLOG(warning, #{msg => "array topic print", mqttTopic => MqttTopic, kafkaTopic => KafkaTopic}),
+    brod:start_producer(client, KafkaTopic, []),
+    func(T).
 
 kafka_init(_Env) ->
     ?SLOG(warning, "Start to init emqx plugin kafka...... ~n"),
@@ -62,75 +60,72 @@ kafka_init(_Env) ->
     KafkaServer = maps:get(<<"kafka_server">>, Conf),
     ?SLOG(warning, #{msg => "conf", conf => Conf}),
 
-    kafkaServers = maps:get(<<"kafka_servers">>),
-    
+    KafkaServers = maps:get(<<"kafka_servers">>, Conf),
+    List = maps:get(<<"topic_mapping">>, Conf),
+    func(List),
     % Port = maps:get(<<"port">>, Conf),
-    % {ok, _} = application:ensure_all_started(brod),
-    % ok = brod:start_client([{KafkaServer, Port}], client),
-    % ?SLOG(info, "Init emqx plugin kafka successfully.....~n"),
+    {ok, _} = application:ensure_all_started(brod),
+    ok = brod:start_client(KafkaServers, client),
+    ?SLOG(info, "Init emqx plugin kafka successfully.....~n"),
     ok.
     
 
 %% Called when the plugin application start
 load(Env) ->
     kafka_init([Env]),
-    hook('client.connect',      {?MODULE, on_client_connect, [Env]}),
-    hook('client.connack',      {?MODULE, on_client_connack, [Env]}),
     hook('client.connected',    {?MODULE, on_client_connected, [Env]}),
     hook('client.disconnected', {?MODULE, on_client_disconnected, [Env]}),
-    hook('client.authenticate', {?MODULE, on_client_authenticate, [Env]}),
-    hook('client.authorize',    {?MODULE, on_client_authorize, [Env]}),
-    hook('client.check_acl',    {?MODULE, on_client_check_acl, [Env]}),
     hook('client.subscribe',    {?MODULE, on_client_subscribe, [Env]}),
     hook('client.unsubscribe',  {?MODULE, on_client_unsubscribe, [Env]}),
     hook('session.created',     {?MODULE, on_session_created, [Env]}),
     hook('session.subscribed',  {?MODULE, on_session_subscribed, [Env]}),
     hook('session.unsubscribed',{?MODULE, on_session_unsubscribed, [Env]}),
-    hook('session.resumed',     {?MODULE, on_session_resumed, [Env]}),
-    hook('session.discarded',   {?MODULE, on_session_discarded, [Env]}),
-    hook('session.takeovered',  {?MODULE, on_session_takeovered, [Env]}),
-    hook('session.terminated',  {?MODULE, on_session_terminated, [Env]}),
-    hook('message.publish',     {?MODULE, on_message_publish, [Env]}),
-    hook('message.delivered',   {?MODULE, on_message_delivered, [Env]}),
-    hook('message.acked',       {?MODULE, on_message_acked, [Env]}),
-    hook('message.dropped',     {?MODULE, on_message_dropped, [Env]}).
+    hook('message.publish',     {?MODULE, on_message_publish, [Env]}).
 
 %%--------------------------------------------------------------------
 %% Client LifeCircle Hooks
 %%--------------------------------------------------------------------
 
-on_client_connect(ConnInfo, Props, _Env) ->
-    %% this is to demo the usage of EMQX's structured-logging macro
-    %% * Recommended to always have a `msg` field,
-    %% * Use underscore instead of space to help log indexers,
-    %% * Try to use static fields
-    ?SLOG(debug, #{msg => "demo_log_msg_on_client_connect",
-                   conninfo => ConnInfo,
-                   props => Props}),
-    {ok, Props}.
-
-on_client_connack(ConnInfo = #{clientid := ClientId}, Rc, Props, _Env) ->
-    io:format("Client(~s) connack, ConnInfo: ~p, Rc: ~p, Props: ~p~n",
-              [ClientId, ConnInfo, Rc, Props]),
-    {ok, Props}.
-
 on_client_connected(ClientInfo = #{clientid := ClientId}, ConnInfo, _Env) ->
-    io:format("Client(~s) connected, ClientInfo:~n~p~n, ConnInfo:~n~p~n",
-              [ClientId, ClientInfo, ConnInfo]).
+    MqttClientConnectedTopic = maps:get(<<"client_connected">>, Conf),
+    Ts = maps:get(<<"connected_at">>, ConnInfo),
+    Username = maps:get(<<"username">>, ConnInfo),
+    Action = <<"connected">>,
+    Keepalive = maps:get(<<"keepalive">>, ConnInfo),
+    {IpAddr, _Port} = maps:get(<<"peername">>, ConnInfo),
+    IsSuperuser = maps:get(<<"is_superuser">>, ConnInfo),
+    Payload = [
+        {action, Action},
+        {username, Username},
+        {keeplive, Keepalive},
+        {ipaddress, iolist_to_binary(ntoa(IpAddr))},
+        {timestamp, Ts},
+        {client_id, ClientId}
+    ],
+    if
+        not IsSuperuser ->
+            send_kafka(Payload, Username, MqttClientConnectedTopic);
+        true -> ok
+    end.
 
 on_client_disconnected(ClientInfo = #{clientid := ClientId}, ReasonCode, ConnInfo, _Env) ->
-  io:format("Client(~s) disconnected due to ~p, ClientInfo:~n~p~n, ConnInfo:~n~p~n",
-              [ClientId, ReasonCode, ClientInfo, ConnInfo]).
-
-on_client_authenticate(ClientInfo = #{clientid := ClientId}, Result, Env) ->
-  io:format("Client(~s) authenticate, ClientInfo:~n~p~n, Result:~p,~nEnv:~p~n",
-    [ClientId, ClientInfo, Result, Env]),
-  {ok, Result}.
-
-on_client_authorize(ClientInfo = #{clientid := ClientId}, PubSub, Topic, Result, Env) ->
-  io:format("Client(~s) authorize, ClientInfo:~n~p~n, ~p to topic(~s) Result:~p,~nEnv:~p~n",
-    [ClientId, ClientInfo, PubSub, Topic, Result, Env]),
-  {ok, Result}.
+    MqttClientDisconnected = maps:get(<<"client_disconnected">>),
+    Ts = maps:get(<<"connected_at">>, ConnInfo),
+    Username = maps:get(<<"username">>, ConnInfo),
+    Action = <<"disconnected">>,
+    IsSuperuser = maps:get(is_superuser, ClientInfo),
+    Payload = [
+        {action, Action},
+        {device_id, Username},
+        {client_id, ClientId},
+        {reason, ReasonCode},
+        {ts, Ts}
+    ],
+    if 
+        not IsSuperuser ->
+            send_kafka(Payload, Username, MqttClientDisconnected);
+        true -> ok
+    end.
 
 on_client_subscribe(#{clientid := ClientId}, _Properties, TopicFilters, _Env) ->
     io:format("Client(~s) will subscribe: ~p~n", [ClientId, TopicFilters]),
@@ -153,19 +148,6 @@ on_session_subscribed(#{clientid := ClientId}, Topic, SubOpts, _Env) ->
 on_session_unsubscribed(#{clientid := ClientId}, Topic, Opts, _Env) ->
     io:format("Session(~s) unsubscribed ~s with opts: ~p~n", [ClientId, Topic, Opts]).
 
-on_session_resumed(#{clientid := ClientId}, SessInfo, _Env) ->
-    io:format("Session(~s) resumed, Session Info:~n~p~n", [ClientId, SessInfo]).
-
-on_session_discarded(_ClientInfo = #{clientid := ClientId}, SessInfo, _Env) ->
-    io:format("Session(~s) is discarded. Session Info: ~p~n", [ClientId, SessInfo]).
-
-on_session_takeovered(_ClientInfo = #{clientid := ClientId}, SessInfo, _Env) ->
-    io:format("Session(~s) is takeovered. Session Info: ~p~n", [ClientId, SessInfo]).
-
-on_session_terminated(_ClientInfo = #{clientid := ClientId}, Reason, SessInfo, _Env) ->
-    io:format("Session(~s) is terminated due to ~p~nSession Info: ~p~n",
-              [ClientId, Reason, SessInfo]).
-
 %%--------------------------------------------------------------------
 %% Message PubSub Hooks
 %%--------------------------------------------------------------------
@@ -175,46 +157,64 @@ on_message_publish(Message = #message{topic = <<"$SYS/", _/binary>>}, _Env) ->
     {ok, Message};
 
 on_message_publish(Message, _Env) ->
-    io:format("Publish ~p~n", [emqx_message:to_map(Message)]),
-    {ok, Message}.
+    Timestamp = Message#message.timestamp,
+    Payload = Message#message.payload,
+    Username = emqx_message:get_header(username, Message),
+    Topic = Message#message.topic,
+    MsgType = <<"publish">>,
+    From = Message#message.from,
+    Qos = Message#message.qos,
+    Retain = emqx_message:get_flag(retain, Message),
+    MsgId = Message#message.id,
 
-on_message_dropped(#message{topic = <<"$SYS/", _/binary>>}, _By, _Reason, _Env) ->
-    ok;
-on_message_dropped(Message, _By = #{node := Node}, Reason, _Env) ->
-    io:format("Message dropped by node ~p due to ~p:~n~p~n",
-              [Node, Reason, emqx_message:to_map(Message)]).
+    Payload1 = json_minify(Payload),
 
-on_message_delivered(_ClientInfo = #{clientid := ClientId}, Message, _Env) ->
-    io:format("Message delivered to client(~s):~n~p~n",
-              [ClientId, emqx_message:to_map(Message)]),
-    {ok, Message}.
+    MsgBody = [
+        {msg_id, binary:encode_hex(MsgId)},
+        {ts, Timestamp},
+        {payload, Payload1},
+        {device_id, Username},
+        {topic, Topic},
+        {action, MsgType},
+        {client_id, From},
+        {qos, Qos},
+        {retain, Retain}
+    ],
 
-on_message_acked(_ClientInfo = #{clientid := ClientId}, Message, _Env) ->
-    io:format("Message acked by client(~s):~n~p~n",
-              [ClientId, emqx_message:to_map(Message)]).
+    TopicStr = binary_to_list(Topic),
+    
+
+
+send_kafka(MsgBody, Username, KafkaTopic) -> 
+    {ok, Mb} = emqx_json:safe_encode(MsgBody),
+    PayloadJson = iolist_to_binary(Mb),
+    brod:produce_cb(client, KafkaTopic, hash, Username, PayloadJson, fun(_,_) -> ok end),
+    ok.
+
+ntoa({0, 0, 0, 0, 0, 16#ffff, AB, CD}) ->
+  inet_parse:ntoa({AB bsr 8, AB rem 256, CD bsr 8, CD rem 256});
+ntoa(IP) ->
+  inet_parse:ntoa(IP).
+
+json_minify(Payload)->
+    IsJson = jsx:is_json(Payload),
+    if 
+         IsJson ->
+            jsx:minify(Payload);
+        true ->
+            Payload
+    end.
 
 %% Called when the plugin application stop
 unload() ->
-    unhook('client.connect',      {?MODULE, on_client_connect}),
-    unhook('client.connack',      {?MODULE, on_client_connack}),
     unhook('client.connected',    {?MODULE, on_client_connected}),
     unhook('client.disconnected', {?MODULE, on_client_disconnected}),
-    unhook('client.authenticate', {?MODULE, on_client_authenticate}),
-    unhook('client.authorize',    {?MODULE, on_client_authorize}),
-    unhook('client.check_acl',    {?MODULE, on_client_check_acl}),
     unhook('client.subscribe',    {?MODULE, on_client_subscribe}),
     unhook('client.unsubscribe',  {?MODULE, on_client_unsubscribe}),
     unhook('session.created',     {?MODULE, on_session_created}),
     unhook('session.subscribed',  {?MODULE, on_session_subscribed}),
     unhook('session.unsubscribed',{?MODULE, on_session_unsubscribed}),
-    unhook('session.resumed',     {?MODULE, on_session_resumed}),
-    unhook('session.discarded',   {?MODULE, on_session_discarded}),
-    unhook('session.takeovered',  {?MODULE, on_session_takeovered}),
-    unhook('session.terminated',  {?MODULE, on_session_terminated}),
-    unhook('message.publish',     {?MODULE, on_message_publish}),
-    unhook('message.delivered',   {?MODULE, on_message_delivered}),
-    unhook('message.acked',       {?MODULE, on_message_acked}),
-    unhook('message.dropped',     {?MODULE, on_message_dropped}).
+    unhook('message.publish',     {?MODULE, on_message_publish}).
 
 hook(HookPoint, MFA) ->
     %% use highest hook priority so this module's callbacks
